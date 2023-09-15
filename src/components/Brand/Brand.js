caf6f94df042bab701710f1ab5b8c23e4064d15a
malpena <63101677+malpena@users.noreply.github.com>
2023-09-15 19:44:19 +1000

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -11,7 +11,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {<h4>SYDNEY</h4>}
+      {<img src="https://zoomllyhues.netlify.app/Zoomllylogofull.jpg">}
       <svg
         width={127}
         height={26}
