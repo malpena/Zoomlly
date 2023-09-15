4059c0e36a8448abf2c000825b951d1d20a6ec08
malpena <63101677+malpena@users.noreply.github.com>
2023-09-15 19:53:37 +1000

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -11,7 +11,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      { <h4>ZOOMLLY HUES - STYLE YOUR WAY!</h4> }
+      { <h4>Zoomlly Hues - Style your way!</h4> }
       <svg
         width={127}
         height={26}
