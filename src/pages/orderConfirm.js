605ac2e9d320c3019650fb987fddd6386c62539b
malpena <63101677+malpena@users.noreply.github.com>
2023-09-15 18:35:21 +1000

    Update orderConfirm.js

--- src/pages/orderConfirm.js
+++ src/pages/orderConfirm.js
@@ -13,7 +13,7 @@ const OrderConfirmPage = (props) => {
           <h1>Thank You!</h1>
           <p>
             We are now processing your order. If you have any concerns feel free
-            to email us at customerservice@example.com
+            to email us at customerservice@zoomlly.com or interact with our <a href = "https://zoomllyhues.netlify.app/">amazing team</a>
           </p>
           <div className={styles.actionContainer}>
             <ActionCard
